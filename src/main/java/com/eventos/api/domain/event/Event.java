8de6253afba330136ca6ac82993d5bf47a24c160
kevintavares33 <kvtavarares30@icloud.com>
2024-07-15 16:02:48 -0300

    feat: repositories

---
+++ src/main/java/com/eventos/api/domain/event/Event.java
@@ -0,0 +1,31 @@
+package com.eventos.api.domain.event;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Table(name = "event")
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Event {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String title;
+    private String description;
+    private String imageurl;
+    private String eventurl;
+    private boolean remote;
+    private String date;
+}
